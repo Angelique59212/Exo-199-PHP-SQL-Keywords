0db9a2e742597c86f3182f9637bd0a7e301af3d2
angel <dehainaut.angelique@orange.fr>
2022-02-25 12:08:04 +0100

    ->order by

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
+require 'Connect.php';
+require 'Config.php';
 
+$myConnexion = Connect::dbConnect();
 
+$stmt = $myConnexion->prepare("SELECT prenom,nom,pays FROM users ");
+
+$stmt = $myConnexion->prepare("SELECT DISTINCT pays FROM users");
+$state = $stmt->execute();
+if ($state) {
+    foreach ($stmt->fetchAll() as $user) {
+        echo "Pays: ".$user['pays']."<br>";
+    }
+}
 
 
 
